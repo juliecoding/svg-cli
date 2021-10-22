3fb1864a9cfdc45374699596c28814b359e2b2a3
juliecoding <julie.akohler@gmail.com>
2021-10-22 08:17:32 -0600

    Added filters

--- cli/filters.go
+++ cli/filters.go
@@ -12,25 +12,37 @@ func initFilters() map[string]filterFunc {
 	return map[string]filterFunc{
 		"blur": blur,
 		"bw": bw,
+		"carlton": carlton,
 		"dawn":	dawn,
-		// "duotone": duotone, // Try to get user input on this one
+		"desaturate": desaturate,
 		"dusk": dusk,
+		"fuzzyTv": fuzzyTv,
+		"ginza": ginza,
 		"hueRotate": hueRotate,
-		// "matrix": matrix,
-		// "moodring": moodring,
+		"instagram": instagram,
+		"matrix": matrix,
+		"montyPython": montyPython,
 		"night": night,
-		// "storm": storm,
+		"pointLight": pointLight,
+		"saturate": saturate,
+		"sepia": sepia,
+		"sunshine": sunshine,
 	}
 }
 
 func blur(canvas svg.SVG) {
-
+	fs := svg.Filterspec{ In: "SourceGraphic", Result: "blur" }
+	canvas.FeGaussianBlur(fs, 3, 3)
 }
 
 
 func bw(canvas svg.SVG) {
 	fs := svg.Filterspec{ In: "SourceGraphic", Result: "bw" }
 	canvas.FeColorMatrixSaturate(fs, 0.05)
+}
+
+func carlton(canvas svg.SVG) {
+	canvas.FeImage("../assets/carlton.gif", "carlton")
 }
 
 func dawn(canvas svg.SVG) {
@@ -44,6 +56,11 @@ func dawn(canvas svg.SVG) {
 	canvas.Animate(fmt.Sprintf("#%s", floodId), "flood-opacity", 0.5, 0, 3, 1)
 }
 
+func desaturate(canvas svg.SVG) {
+	fs := svg.Filterspec{ In: "SourceGraphic", Result: "desaturate" }
+	canvas.FeColorMatrixSaturate(fs, 0.3)
+}
+
 func dusk(canvas svg.SVG) {
 	result := "dusk"
 	floodId := "__duskFlood"
@@ -55,9 +72,42 @@ func dusk(canvas svg.SVG) {
 	canvas.Animate(fmt.Sprintf("#%s", floodId), "flood-opacity", 0.3, 0.1, 9, 1)
 }
 
+func fuzzyTv(canvas svg.SVG) {
+	fs := svg.Filterspec{ In: "SourceGraphic", Result: "fuzzyTv" }
+	canvas.FeConvolveMatrix(fs, [9]int{-1, -1, -1, -1, 4, 0, 0, 0, -1})
+}
+
+func ginza(canvas svg.SVG) {
+	fscm := svg.Filterspec{ In: "SourceGraphic", Result: "ginza" }
+
+	matrix := [20]float64{
+		0.8786, 0.1538, 0.4, 0, 0,
+		0.0698, 0.9372, 0.0336, 0, 0,
+		0.0544, 0.1068, 0.8262, 0, 0,
+		0, 0, 0, 1, 0,
+	}
+	canvas.FeColorMatrix(fscm, matrix)
+
+}
+
 func hueRotate(canvas svg.SVG) {
 	fs := svg.Filterspec{ In: "SourceGraphic", Result: "hueRotate" }
 	canvas.FeColorMatrixHue(fs, 180)
+}
+
+func instagram(canvas svg.SVG) {
+	fs := svg.Filterspec{ In: "SourceGraphic", Result: "instagram" }
+	canvas.FeColorMatrixHue(fs, 20)
+}
+
+func matrix(canvas svg.SVG) {
+	canvas.FeImage("../assets/matrixCascade.png", "matrix")
+}
+
+func montyPython(canvas svg.SVG) {
+	fs := svg.Filterspec{ Result: "montyPython" }
+	color := getUserInput("What is your favorite color?! (I'm a witch, so in Hex, please!)")
+	canvas.FeFlood(fs, color, 0.5)
 }
 
 func night(canvas svg.SVG) {
@@ -69,3 +119,36 @@ func night(canvas svg.SVG) {
 	canvas.FeBlend(blendSpec, "multiply")
 	canvas.Animate(fmt.Sprintf("#%s", floodId), "flood-opacity", 0, 0.95, 9, 1)
 }
+
+func pointLight(canvas svg.SVG) {
+	fs := svg.Filterspec{ In: "SourceGraphic", Result: "pointLight" }
+	canvas.FeSpecularLighting(fs, 0.5, 1.5, 80, "#F3F3F0")
+	canvas.FePointLight(100, 100, 500)
+	canvas.FeSpecEnd()
+}
+
+func saturate(canvas svg.SVG) {
+	fs := svg.Filterspec{ In: "SourceGraphic", Result: "saturate" }
+	canvas.FeColorMatrixSaturate(fs, 3)
+}
+
+func sepia(canvas svg.SVG) {
+	fs := svg.Filterspec{ In: "SourceGraphic", Result: "sepia" }
+	matrix := [20]float64{
+		0.280, 0.5, 0.25, 0, 0,
+		0.140, 0.4, 0.09, 0, 0,
+		0.080, 0.3, 0.03, 0, 0,
+		0, 0, 0, 1, 0,
+	}
+	canvas.FeColorMatrix(fs, matrix)
+}
+
+func sunshine(canvas svg.SVG) {
+	fs := svg.Filterspec{ In: "SourceGraphic", Result: "moonbeam" }
+	canvas.FeSpecularLighting(fs, 0.5, 1.5, 80, "#fff9c4")
+	canvas.FePointLight(50, 50, 800)
+	canvas.FePointLight(-50, -50, 800)
+	canvas.FeSpecEnd()
+}
+
+// pointLight saturate sepia blur dawn desaturate dusk fuzzyTv ginza hueRotate matrix night
